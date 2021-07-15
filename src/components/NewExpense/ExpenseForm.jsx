e15027efc328e8b5d5032e05d584bc6e7121ab4f
timstetter <tstetter@gmail.com>
2021-07-15 06:19:09 -0400

    added ExpensesChart

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -24,7 +24,7 @@ const ExpenseForm = (props) => {
 
     const ExpenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
