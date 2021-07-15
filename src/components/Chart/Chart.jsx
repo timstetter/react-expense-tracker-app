210ef58cf090fc46735a0ceaf40d5aa1726683dc
timstetter <tstetter@gmail.com>
2021-07-15 06:35:25 -0400

    fixed logic for max in Chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -5,7 +5,7 @@ import "./Chart.css";
 
 const Chart = (props) => {
   const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
-  const totalMaximum = Math.max(...dataPointValues);
+  const totalMaximum = dataPointValues.reduce((a, b) => a + b, 0);
 
   return (
     <div className="chart">
